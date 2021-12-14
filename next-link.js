a4e9c38ea1851e950306a959fe3d4d8fd1542315
Shrroy <33762501+Shrroy@users.noreply.github.com>
2021-12-14 03:35:24 +0100

    Update next-link.js

--- next-link.js
+++ next-link.js
@@ -2,7 +2,7 @@ import LinkNext from 'next/link';
 
 const Link = (props) =>{
     const { href,to,className, style, children } = props;
-    return <LinkNext href={href || to} passHref {...props}><a {...className} {...style}>{children}</a></LinkNext>
+    return <LinkNext href={href || to} passHref {...props}><a className={className || ''} style={style || {} }>{children}</a></LinkNext>
 }
 
 export default Link;
